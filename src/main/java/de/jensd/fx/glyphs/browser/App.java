7debbb5c4eff2ae8e00c5f4f2b954f29d2a24dc9
Shadow Walker <thiensu_huyenthoai_rai@yahoo.com>
2020-02-01 15:24:59 +0700

    initial commit

---
+++ src/main/java/de/jensd/fx/glyphs/browser/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jensd.fx.glyphs.browser;
+
+/**
+ *
+ * @author ShadowWalker
+ */
+public class App {
+    public static void main(String[] args) {
+        GlyphsBrowserApp.main(args);
+    }
+}
